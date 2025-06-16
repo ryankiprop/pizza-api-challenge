1d453e3725453a324ba8ef817c12e3fd1dea9e21
langatjnr <ryanlangatjnr@gmail.com>
2025-06-16 11:51:25 +0300

    pizza model created

---
+++ server/models/pizza.py
@@ -0,0 +1,20 @@
+from server import db
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    ingredients = db.Column(db.String, nullable=False)
+    
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
+    
+    def __repr__(self):
+        return f'<Pizza {self.name}>'
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'ingredients': self.ingredients
+        }
