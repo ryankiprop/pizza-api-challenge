c6f61c32320464b19211fd5dda29c09e928cd04d
langatjnr <ryanlangatjnr@gmail.com>
2025-06-16 12:15:11 +0300

    updated app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,14 @@
 from server import create_app, db
+from server.controllers.restaurant_controller import restaurant_bp
+from server.controllers.pizza_controller import pizza_bp
+from server.controllers.restaurant_pizza_controller import restaurant_pizza_bp
 
 app = create_app()
+
+# Register blueprints
+app.register_blueprint(restaurant_bp, url_prefix='/api')
+app.register_blueprint(pizza_bp, url_prefix='/api')
+app.register_blueprint(restaurant_pizza_bp, url_prefix='/api')
 
 @app.route('/')
 def home():
